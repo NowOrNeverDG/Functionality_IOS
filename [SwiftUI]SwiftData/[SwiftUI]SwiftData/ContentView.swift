2c752e449c45baa7a2320a1e9552df840cbf0c2a
NowOrNeverDG <447405890@qq.com>
2023-12-01 12:00:46 -0500

    updated SwiftData: Add search functionality

--- [SwiftUI]SwiftData/[SwiftUI]SwiftData/ContentView.swift
+++ [SwiftUI]SwiftData/[SwiftUI]SwiftData/ContentView.swift
@@ -12,44 +12,95 @@ struct ContentView: View {
     
     /// 3. Create a environment property
     /*
-     We are going to specify the pathe of the context for working with data.
+     We are going to specify the path of the context for working with data.
      taking items from persisitent store and putting it into memory.
     */
     @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var isPresented: Bool = false
     
     /// 4. add object and query to get the data
     @Query private var items: [DataItem]
+    @State private var filterItems: [DataItem] = []
+    
+    
+    @State private var searchText = ""
     
     var body: some View {
-        VStack {
-            Text("Tap on this button to add data")
-            Button("Add an Button") {
-                addItem()
-            }
-            
-            List {
-                ForEach(items) { item in
-                    HStack {
-                        Text(item.name)
-                        Spacer()
-                        Button {
-                            updateItem(item)
-                        } label: {
-                            Image(systemName: "arrow.3.trianglepath")
+        NavigationView {
+            VStack {
+                TextField(" Search name", text: $searchText)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(lineWidth: 0.5)
+                    }
+                    .onChange(of: searchText) {
+                        if searchText == "" {
+                            filterItems = items
+                        } else {
+                            filterItems = items.filter{ $0.name.contains(searchText) }
+                        }
+                    }
+                    .padding()
+                
+                
+                List {
+                    ForEach(filterItems) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Button {
+                                updateItem(item)
+                            } label: {
+                                Image(systemName: "arrow.3.trianglepath")
+                            }
+                        }
+                    }
+                    .onDelete{ indexes in
+                        for index in indexes {
+                            deleteItem(items[index])
                         }
                     }
                 }
-                .onDelete{ indexes in
-                    for index in indexes {
-                        deleteItem(items[index])
-                    }
+            }
+            .toolbar {
+                Button {
+                    isPresented.toggle()
+                } label: {
+                    Image(systemName: "plus.app")
                 }
             }
         }
+        .sheet(isPresented: $isPresented) {
+            presentView()
+        }
+        .onAppear{
+            filterItems = items
+        }
     }
     
-    func addItem() {
-        let item = DataItem(name: "Test Item")
+    @State private var nameText: String = ""
+    @ViewBuilder
+    func presentView() -> some View {
+        VStack {
+            TextField(" Search name", text: $nameText)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(lineWidth: 0.5)
+                }
+                .padding()
+            
+            Button("Submit") {
+                isPresented.toggle()
+                addItem(nameText)
+            }
+        }
+
+        
+    }
+    func addItem(_ name: String) {
+        let item = DataItem(name: name)
         context.insert(item)
     }
     
